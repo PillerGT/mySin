72cad6925c26d92605c8db6169da4c68329bdcb0
Endogenesis <endoCloud@MacBook-Pro-NIkolay.local>
2015-01-17 00:10:39 +0300

    Initial Commit

---
+++ mySin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mySin
+//
+//  Created by NIkolay Tsygankov on 17.01.15.
+//  Copyright (c) 2015 iosdevcourse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mySin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mySin
+//
+//  Created by NIkolay Tsygankov on 17.01.15.
+//  Copyright (c) 2015 iosdevcourse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
